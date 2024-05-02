eff3d62655c3a9dfe941a7f35448a5d8e86e3eb8
dinorain <djourdan555@gmail.com>
2024-05-02 11:22:37 +0700

    fix: test

--- backend/crates/services/web-server/src/web/routes_user.rs
+++ backend/crates/services/web-server/src/web/routes_user.rs
@@ -1,11 +1,13 @@
 use crate::web::{self, remove_token_cookie, Error, Result};
 use axum::extract::State;
 use axum::routing::post;
+use axum::routing::get;
 use axum::{Json, Router};
 use lib_auth::pwd::{self, ContentToHash, SchemeStatus};
 use lib_core::ctx::Ctx;
 use lib_core::model::user::{UserBmc, UserForLogin};
 use lib_core::model::ModelManager;
+use serde::Serialize;
 use serde::Deserialize;
 use serde_json::{json, Value};
 use tower_cookies::Cookies;
@@ -13,10 +15,28 @@ use tracing::debug;
 
 pub fn routes(mm: ModelManager) -> Router {
 	Router::new()
+		.route("/api/health", get(api_health_handler))
+		
 		.route("/api/login", post(api_login_handler))
 		.route("/api/logout", post(api_logout_handler))
 		.route("/api/register", post(api_register_handler))
+
+		.route("/api/product/list", post(api_product_list_handler))
+		.route("/api/product/detail", post(api_product_detail_handler))
 		.with_state(mm)
+}
+
+async fn api_health_handler() -> Json<Value> {
+	debug!("{:<12} - api_health_handler", "HANDLER");
+
+	// Create the success body.
+	let body = Json(json!({
+		"result": {
+			"success": true
+		}
+	}));
+
+	body
 }
 
 // region:    --- Login
@@ -113,12 +133,26 @@ async fn api_register_handler(
 	debug!("{:<12} - api_register_handler", "HANDLER");
 
 	let RegisterPayload {
-		username,
-		pwd: pwd_clear,
+		email,
+		phone,
+		full_name,
+		gender,
+		birth_date,
+		address,
+		marital_status,
+		occupation,
+		income,
+		dependents,
+		region,
+		familiarity,
+		interests,
 	} = payload;
-	
+
+	// Create the success body.
 	let body = Json(json!({
-		"success": true
+		"result": {
+			"success": true
+		}
 	}));
 
 	Ok(body)
@@ -126,7 +160,129 @@ async fn api_register_handler(
 
 #[derive(Debug, Deserialize)]
 struct RegisterPayload {
-	username: String,
-	pwd: String,
+	email: String,
+	phone: String,
+	full_name: String,
+	gender: String,
+	birth_date: String,
+	address: String,
+	marital_status: String,
+	occupation: String,
+	income: f64,
+	dependents: u32,
+	region: String,
+	familiarity: String,
+	interests: String,
 }
 // endregion: --- Register
+
+
+// region:    --- Product List 
+async fn api_product_list_handler(
+    State(_mm): State<ModelManager>,
+    _cookies: Cookies,
+	Json(payload): Json<ProductListPayload>,
+) -> Result<Json<Value>> {
+	debug!("{:<12} - api_register_handler", "HANDLER");
+
+    // Create a vector to hold the dummy product data
+    let mut rows = Vec::new();
+    for i in 0..8 {
+        rows.push(
+			ProductResponse {
+				id: (i + 1).to_string(),
+				name: "Dummy Product".to_string(),
+				desc: "This is a dummy product description.".to_string(),
+				count_claim: 10,
+				count_review: 20,
+				rating: 4.0,
+				company: "Dummy Company".to_string(),
+				company_logo: "https://dummycompany.com/logo.png".to_string(),
+				banner: "https://dummycompany.com/banner.png".to_string(),
+			}
+		);
+    }
+
+    // Create the success body with the dummy product data
+	let response = ProductListResponse {
+		success: true,
+		rows,
+	};
+	let body = Json(serde_json::to_value(response)?);
+
+    Ok(body)
+}
+
+#[derive(Debug, Deserialize)]
+struct ProductListPayload {
+	search: Option<String>,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ProductResponse {
+	id: String,
+	name: String,
+	desc: String,
+	count_claim: u32,
+	count_review: u32,
+	rating: f64,
+	company: String,
+	company_logo: String,
+	banner: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ProductListResponse {
+	success: bool,
+	rows: Vec<ProductResponse>,
+}
+// endregion: --- Product List
+
+// region:    --- Product Detail
+async fn api_product_detail_handler(
+	State(_mm): State<ModelManager>,
+	_cookies: Cookies,
+	Json(payload): Json<ProductDetailPayload>,
+) -> Result<Json<Value>> {
+	debug!("{:<12} - api_product_detail_handler", "HANDLER");
+
+	let ProductDetailPayload { id } = payload;
+
+	// Create the success body with the dummy product data
+	let response = ProductDetailResponse {
+		success: true,
+		id,
+		name: "Dummy Product".to_string(),
+		desc: "This is a dummy product description.".to_string(),
+		count_claim: 10,
+		count_review: 20,
+		rating: 4.0,
+		company: "Dummy Company".to_string(),
+		company_logo: "https://dummycompany.com/logo.png".to_string(),
+		banner: "https://dummycompany.com/banner.png".to_string(),
+	};
+	let body = Json(serde_json::to_value(response)?);
+
+	Ok(body)
+}
+
+#[derive(Debug, Deserialize)]
+struct ProductDetailPayload {
+	id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ProductDetailResponse {
+	success: bool,
+	id: String,
+	name: String,
+	desc: String,
+	count_claim: u32,
+	count_review: u32,
+	rating: f64,
+	company: String,
+	company_logo: String,
+	banner: String,
+}
+// endregion: --- Product Detail
